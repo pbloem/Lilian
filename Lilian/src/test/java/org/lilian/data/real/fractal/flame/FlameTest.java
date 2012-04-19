9de29a0b3d58a3db37c36cb8d37ebe6fe1860754
Peter Bloem <p@peterbloem.nl>
2012-04-19 14:05:23 +0200

    eliminating expensive tests

--- Lilian/src/test/java/org/lilian/data/real/fractal/flame/FlameTest.java
+++ Lilian/src/test/java/org/lilian/data/real/fractal/flame/FlameTest.java
@@ -23,118 +23,118 @@ import org.lilian.util.Series;
 
 public class FlameTest
 {
-
-	private static final long STEPS = (long)2E9;
-	private static final int XRES = 1920;
-	private static final int YRES = 1080;
-	private static final double[] X_RANGE = new double[]{-16.0/9.0, 16.0/9.0};
-	private static final double[] Y_RANGE = new double[]{-1.0, 1.0};	
-	private static final int OSA = 3;
-	private static int K = 2;
-	private static int FRAMES = 360;
-	private static double VAR = 0.1;
-	private static double VAR_PERTURB = 0.0001;
-	private static boolean WRAP = false;
-	
-	@Test
-	public void testFlame()
-	{
-		String name = "gutenberg7";
-		File dir = new File("/home/peter/Documents/PhD/output/flame/" + name + "/");
-		dir.mkdirs();
-		
-		// long seed = 5692776151140015461l;
-		// long seed = -2654647857798219730l;
-		long seed = new Random().nextLong();
-		System.out.println("seed: " + seed);
-		
-		Global.random = new Random(seed);
-		
-		Builder<Flame> builder = Flame.builder(K);
-		List<Double> change = new ArrayList<Double>(builder.numParameters());
-		List<Double> params = new ArrayList<Double>(builder.numParameters());		
-		
-		for(int i : Series.series(builder.numParameters()))
-		{
-			change.add(0.0);
-			params.add(Global.random.nextDouble() < VAR ? 1.0 : 0.0);			
-		}
-		
-		Flame flame = builder.build(params);
-		for(int i = 0; i < FRAMES; i++)
-		{
-			perturb(change, Math.pow((FRAMES - i)/(double)FRAMES, 10) * VAR_PERTURB);
-			add(params, change);
-			
-			flame = builder.build(params);			
-			
-			Functions.tic();
-			BufferedImage image = flame.draw(STEPS, XRES, YRES, X_RANGE, Y_RANGE, OSA, WRAP);
-			System.out.println(i + ": " + Functions.toc() + "seconds");
-			
-			try
-			{
-				ImageIO.write(image, "PNG", new File(dir, String.format("out%04d.png", i)));
-			} catch (IOException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}	
-	}
-	
-	// @Test
-	public void testFlameEllipse()
-	{
-		String name = "ellipse4";
-		File dir = new File("/home/peter/Documents/PhD/output/flame/" + name + "/");
-		dir.mkdirs();
-		
-		// long seed = 5692776151140015461l;
-		// long seed = -2654647857798219730l;
-		long seed = new Random().nextLong();
-		System.out.println("seed: " + seed);
-		
-		Global.random = new Random(seed);
-		
-		Builder<Flame> builder = Flame.builder(K);
-		Ellipse ellipse = Ellipse.random(builder.numParameters(), VAR);
-				
-		System.out.println(builder.numParameters());
-		System.exit(0);
-		
-		int i = 0;
-		for(double a = 0.0; a < 2*Math.PI; a += (2*Math.PI)/FRAMES)
-		{
-			Flame flame = builder.build(ellipse.point(a));
-			
-			Functions.tic();
-			BufferedImage image = flame.draw(STEPS, XRES, YRES, X_RANGE, Y_RANGE, OSA, WRAP);
-			System.out.println(i + ": " + Functions.toc() + "seconds");
-			
-			try
-			{
-				ImageIO.write(image, "PNG", new File(dir, String.format("out%04d.png", i)));
-			} catch (IOException e)
-			{
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-			i++;
-		}	
-	}	
-	
-	private static void add(List<Double> in, List<Double> change)
-	{
-		for(int i : Series.series(in.size()))
-			in.set(i, in.get(i) + change.get(i));
-	}	
-	
-	private static void perturb(List<Double> in, double var)
-	{
-		for(int i : Series.series(in.size()))
-			in.set(i, in.get(i) + Global.random.nextGaussian() * var);
-	}
+//
+//	private static final long STEPS = (long)2E9;
+//	private static final int XRES = 1920;
+//	private static final int YRES = 1080;
+//	private static final double[] X_RANGE = new double[]{-16.0/9.0, 16.0/9.0};
+//	private static final double[] Y_RANGE = new double[]{-1.0, 1.0};	
+//	private static final int OSA = 3;
+//	private static int K = 2;
+//	private static int FRAMES = 360;
+//	private static double VAR = 0.1;
+//	private static double VAR_PERTURB = 0.0001;
+//	private static boolean WRAP = false;
+//	
+//	@Test
+//	public void testFlame()
+//	{
+//		String name = "gutenberg7";
+//		File dir = new File("/home/peter/Documents/PhD/output/flame/" + name + "/");
+//		dir.mkdirs();
+//		
+//		// long seed = 5692776151140015461l;
+//		// long seed = -2654647857798219730l;
+//		long seed = new Random().nextLong();
+//		System.out.println("seed: " + seed);
+//		
+//		Global.random = new Random(seed);
+//		
+//		Builder<Flame> builder = Flame.builder(K);
+//		List<Double> change = new ArrayList<Double>(builder.numParameters());
+//		List<Double> params = new ArrayList<Double>(builder.numParameters());		
+//		
+//		for(int i : Series.series(builder.numParameters()))
+//		{
+//			change.add(0.0);
+//			params.add(Global.random.nextDouble() < VAR ? 1.0 : 0.0);			
+//		}
+//		
+//		Flame flame = builder.build(params);
+//		for(int i = 0; i < FRAMES; i++)
+//		{
+//			perturb(change, Math.pow((FRAMES - i)/(double)FRAMES, 10) * VAR_PERTURB);
+//			add(params, change);
+//			
+//			flame = builder.build(params);			
+//			
+//			Functions.tic();
+//			BufferedImage image = flame.draw(STEPS, XRES, YRES, X_RANGE, Y_RANGE, OSA, WRAP);
+//			System.out.println(i + ": " + Functions.toc() + "seconds");
+//			
+//			try
+//			{
+//				ImageIO.write(image, "PNG", new File(dir, String.format("out%04d.png", i)));
+//			} catch (IOException e)
+//			{
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}	
+//	}
+//	
+//	// @Test
+//	public void testFlameEllipse()
+//	{
+//		String name = "ellipse4";
+//		File dir = new File("/home/peter/Documents/PhD/output/flame/" + name + "/");
+//		dir.mkdirs();
+//		
+//		// long seed = 5692776151140015461l;
+//		// long seed = -2654647857798219730l;
+//		long seed = new Random().nextLong();
+//		System.out.println("seed: " + seed);
+//		
+//		Global.random = new Random(seed);
+//		
+//		Builder<Flame> builder = Flame.builder(K);
+//		Ellipse ellipse = Ellipse.random(builder.numParameters(), VAR);
+//				
+//		System.out.println(builder.numParameters());
+//		System.exit(0);
+//		
+//		int i = 0;
+//		for(double a = 0.0; a < 2*Math.PI; a += (2*Math.PI)/FRAMES)
+//		{
+//			Flame flame = builder.build(ellipse.point(a));
+//			
+//			Functions.tic();
+//			BufferedImage image = flame.draw(STEPS, XRES, YRES, X_RANGE, Y_RANGE, OSA, WRAP);
+//			System.out.println(i + ": " + Functions.toc() + "seconds");
+//			
+//			try
+//			{
+//				ImageIO.write(image, "PNG", new File(dir, String.format("out%04d.png", i)));
+//			} catch (IOException e)
+//			{
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//			
+//			i++;
+//		}	
+//	}	
+//	
+//	private static void add(List<Double> in, List<Double> change)
+//	{
+//		for(int i : Series.series(in.size()))
+//			in.set(i, in.get(i) + change.get(i));
+//	}	
+//	
+//	private static void perturb(List<Double> in, double var)
+//	{
+//		for(int i : Series.series(in.size()))
+//			in.set(i, in.get(i) + Global.random.nextGaussian() * var);
+//	}
 
 }
--- Lilian/src/test/java/org/lilian/search/evo/ESFlameTest.java
+++ Lilian/src/test/java/org/lilian/search/evo/ESFlameTest.java
@@ -40,69 +40,69 @@ public class ESFlameTest
 	private static int K = 2;
 	private static boolean WRAP = false;
 	
-	@Test
-	public void testIFS()
-	{
-		String name = "example_3";
-		File dir = new File("/home/peter/Documents/PhD/output/es_flame/" + name + "/");
-		dir.mkdirs();
-		
-		// MSTarget target = new MSTarget(STEPS, XRES, YRES, OSA, WRAP, X_RANGE, Y_RANGE); 
-//		FacticityTarget target = new FacticityTarget(STEPS, XRES, YRES, OSA, WRAP, X_RANGE, Y_RANGE);
-		
-		RenderedImage targetImage = null;
-		try
-		{
-			targetImage = ImageIO.read(new File("/home/peter/Documents/PhD/output/es_flame/example_1/jennie.jpg"));
-		} catch (IOException e1)
-		{
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		ExampleTarget target = new ExampleTarget(targetImage, STEPS, XRES, YRES, OSA, WRAP, X_RANGE, Y_RANGE);
-//		System.out.println("target max = " + target.max());
-		
-		Builder<Flame> builder = Flame.builder(K);
-		List<List<Double>> initial = ES.initial(POP_SIZE, builder.numParameters(), VAR);
-		
-		ES<Flame> es = new ES<Flame>(
-				builder, target, initial, 
-				2, initial.size() * 2, 0, 
-				ES.CrossoverMode.UNIFORM);
-		
-		Functions.tic();		
-		for(int i : Series.series(GENERATIONS))
-		{
-			es.breed();
-
-			if(true)//i%(GENERATIONS/100) == 0)
-			{
-				Flame best = es.best().instance();
-				
-				BufferedImage image = best.draw(STEPS, XRES, YRES, X_RANGE, Y_RANGE, OSA, WRAP);
-				try
-				{
-					ImageIO.write(image, "PNG", new File(dir, String.format("out%04d", i)));
-				} catch (IOException e)
-				{
-					e.printStackTrace();
-				}
-				
-				if(i % (GENERATIONS/25) == 0 && i != 0)
-				{
-					image = best.draw((long)1e9, 1920, 1080, X_RANGE, Y_RANGE, 3, WRAP);
-					try
-					{
-						ImageIO.write(image, "PNG", new File(dir, String.format("out%04d.big", i)));
-					} catch (IOException e)
-					{
-						e.printStackTrace();
-					}
-				}
-				
-				System.out.println("generation " + i + ": " + Functions.toc() + " seconds, fitness:" + es.best().fitness());
-				Functions.tic();				
-			}	
-		}
-	}
+//	@Test
+//	public void testIFS()
+//	{
+//		String name = "example_3";
+//		File dir = new File("/home/peter/Documents/PhD/output/es_flame/" + name + "/");
+//		dir.mkdirs();
+//		
+//		// MSTarget target = new MSTarget(STEPS, XRES, YRES, OSA, WRAP, X_RANGE, Y_RANGE); 
+////		FacticityTarget target = new FacticityTarget(STEPS, XRES, YRES, OSA, WRAP, X_RANGE, Y_RANGE);
+//		
+//		RenderedImage targetImage = null;
+//		try
+//		{
+//			targetImage = ImageIO.read(new File("/home/peter/Documents/PhD/output/es_flame/example_1/jennie.jpg"));
+//		} catch (IOException e1)
+//		{
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//		ExampleTarget target = new ExampleTarget(targetImage, STEPS, XRES, YRES, OSA, WRAP, X_RANGE, Y_RANGE);
+////		System.out.println("target max = " + target.max());
+//		
+//		Builder<Flame> builder = Flame.builder(K);
+//		List<List<Double>> initial = ES.initial(POP_SIZE, builder.numParameters(), VAR);
+//		
+//		ES<Flame> es = new ES<Flame>(
+//				builder, target, initial, 
+//				2, initial.size() * 2, 0, 
+//				ES.CrossoverMode.UNIFORM);
+//		
+//		Functions.tic();		
+//		for(int i : Series.series(GENERATIONS))
+//		{
+//			es.breed();
+//
+//			if(true)//i%(GENERATIONS/100) == 0)
+//			{
+//				Flame best = es.best().instance();
+//				
+//				BufferedImage image = best.draw(STEPS, XRES, YRES, X_RANGE, Y_RANGE, OSA, WRAP);
+//				try
+//				{
+//					ImageIO.write(image, "PNG", new File(dir, String.format("out%04d", i)));
+//				} catch (IOException e)
+//				{
+//					e.printStackTrace();
+//				}
+//				
+//				if(i % (GENERATIONS/25) == 0 && i != 0)
+//				{
+//					image = best.draw((long)1e9, 1920, 1080, X_RANGE, Y_RANGE, 3, WRAP);
+//					try
+//					{
+//						ImageIO.write(image, "PNG", new File(dir, String.format("out%04d.big", i)));
+//					} catch (IOException e)
+//					{
+//						e.printStackTrace();
+//					}
+//				}
+//				
+//				System.out.println("generation " + i + ": " + Functions.toc() + " seconds, fitness:" + es.best().fitness());
+//				Functions.tic();				
+//			}	
+//		}
+//	}
 }
