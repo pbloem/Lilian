dcd0f99b61760c1f3519b0131cfb66631d9ade5a
Peter Bloem <p@peterbloem.nl>
2012-11-02 16:43:11 +0100

    ...

--- Lilian/src/main/java/org/lilian/data/real/Generators.java
+++ Lilian/src/main/java/org/lilian/data/real/Generators.java
@@ -3,6 +3,11 @@ package org.lilian.data.real;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 
+import java.util.List;
+
+import org.apache.commons.math.linear.ArrayRealVector;
+import org.apache.commons.math.linear.RealVector;
+
 public class Generators
 {
 
@@ -80,4 +85,74 @@ public class Generators
 		
 	}	
 	
+	public static Generator<Point> rossler()
+	{
+		return rossler(0.2, 0.2, 5.7, 0.001);
+	}
+	
+	public static Generator<Point> rossler(double a, double b, double c, double h)
+	{
+		Generator<Point> rossler = new RK4(new RosslerDerivative(a, b, c), 0.001, new ArrayRealVector(3));
+		
+		rossler.generate(1000000);
+		
+		return rossler;
+	}
+	
+	private static class RosslerDerivative implements RK4.Derivative
+	{
+		double a, b, c;
+		
+		public RosslerDerivative(double a, double b, double c)
+		{
+			this.a = a;
+			this.b = b;
+			this.c = c;
+		}
+		
+		@Override
+		public RealVector derivative(RealVector in, double t)
+		{
+			double x = in.getEntry(0),
+			       y = in.getEntry(1),
+			       z = in.getEntry(2);
+			
+			return new ArrayRealVector(new double[]{
+				- y - z,
+				x + a * y,
+				b + z * (x - c)
+			}, 0, 3);
+		}
+	}
+	
+	public static Generator<Point> mapped(Generator<Point> master, Map map)
+	{
+		return new MappedGenerator(map, master);
+	}
+	
+	public static class MappedGenerator implements Generator<Point>
+	{
+		private Map map;
+		private Generator<Point> master;
+
+		public MappedGenerator(Map map, Generator<Point> master)
+		{
+			this.map = map;
+			this.master = master;
+		}
+
+		@Override
+		public Point generate()
+		{
+			Point p = master.generate();
+			return map.map(p);
+		}
+
+		@Override
+		public List<Point> generate(int n)
+		{
+			List<Point> p = master.generate(n);
+			return map.map(p);
+		}
+	}
 }
---
+++ Lilian/src/main/java/org/lilian/data/real/RK4.java
@@ -0,0 +1,49 @@
+package org.lilian.data.real;
+
+import org.apache.commons.math.linear.RealVector;
+
+/**
+ * Implementation of the Runge Kutta method for approximating the trajectory
+ * of a differential equation.
+ * 
+ * @author Peter
+ *
+ */
+public class RK4 extends AbstractGenerator<Point>
+{
+	private Derivative d;
+	private double h;
+	private RealVector x;
+	private double t = 0.0;
+	
+	public RK4(Derivative derivative, double h, RealVector initial)
+	{
+		this.d = derivative;
+		this.h = h;
+		this.x = initial;
+	}
+	
+	public static interface Derivative
+	{
+		public RealVector derivative(RealVector x, double t);
+	}
+
+	@Override
+	public Point generate()
+	{		
+		RealVector k1 = d.derivative(x, t).mapMultiply(h),
+		           k2 = d.derivative(x.add(k1.mapMultiply(0.5)), t + 0.5 * h).mapMultiply(h),
+		           k3 = d.derivative(x.add(k2.mapMultiply(0.5)), t + 0.5 * h).mapMultiply(h),
+		           k4 = d.derivative(x.add(k3), t + h).mapMultiply(h);
+		
+		RealVector k = k1
+				.add(k2.mapMultiply(2.0))
+				.add(k3.mapMultiply(2.0))
+				.add(k4);
+		
+		x = x.add(k.mapMultiply(1.0/6.0));
+		t = t + h;
+		
+		return new Point(x); 
+	}
+}
