0683539c3a72c8db8ddfac9d475b297cb3a6cc70
Peter Bloem <p@peterbloem.nl>
2013-12-13 20:26:52 +0100

    ...

--- Lilian/src/main/java/org/lilian/rdf/SBSimplifier.java
+++ Lilian/src/main/java/org/lilian/rdf/SBSimplifier.java
@@ -31,57 +31,70 @@ public class SBSimplifier<L, T>
 {
 	private DTGraph<L, T> graph;
 	private ConnectionClustering<L> cc;
+	private boolean keepHubs;
+	private int k, i = 0;
 	
 	/**
 	 * Creats an SBSimplifier for a given graph. The graph is copied, and the 
 	 * resulting Simplifier is not backed by the graph.
+	 * 
 	 * @param graph
+	 * @param k The number of hubs to remove each iteration (0.005 of the number of nodes is a good rule of thumb)
+	 * @param keepHubs If true, the hub node is kept and replicated for every 
+	 *   position in the graph it occurs. If false, it is simply deleted. 
 	 */
-	public SBSimplifier(DTGraph<L, T> graph)
+	public SBSimplifier(DTGraph<L, T> graph, int k, boolean keepHubs)
 	{
 		this.graph = MapDTGraph.copy(graph);
 		cc = new ConnectionClustering<L>(graph);
+		this.k = k;
+		this.keepHubs = keepHubs;
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void iterate()
-	{
-		Comparator<Node<L>> comp = new DegreeComparator<L>();
-		MaxObserver<Node<L>> observer = new MaxObserver<Node<L>>(1, comp);
+	{	
+		cc = new ConnectionClustering<L>(graph);
+		int gccSize =  cc.largestCluster().size();
+		Global.log().info(i + ") GCC: " + gccSize + ", components: " + cc.numClusters());
 
+		if(gccSize < k)
+			throw new RuntimeException("The largest connected component is smaller than c");
+		
+		Comparator<Node<L>> comp = new DegreeComparator<L>();
+		MaxObserver<Node<L>> observer = new MaxObserver<Node<L>>(k, comp);
 		for (int index : cc.largestCluster())
 			observer.observe(graph.get(index));
 
-		@SuppressWarnings("unchecked")
-		DTNode<L, T> top = (DTNode<L, T>) observer
-				.elements().get(0);
+		Global.log().info("Current top hubs: " + observer.elements());
 
-		Global.log().info("Current top hub: " + top + " (d: " + top.degree() + ")");
-
-		for (DTLink<L, T> link : top.links())
-		{
-			// * ignore self-links
-			if (link.from().equals(link.to()))
-				continue;
-
-			DTNode<L, T> neighbor = link.other(top);
-			DTNode<L, T> newNode = graph.add(top.label());
-
-			if (link.from().equals(top))
-				newNode.connect(neighbor, link.tag());
-			else
-				neighbor.connect(newNode, link.tag());
-
-			link.remove();
-		}
-
-		top.remove();
+		for(Node<L> hub : observer.elements())
+			remove((DTNode<L, T>)hub);
 		
+		i++;
 	}
 	
-	public void cleanLeaves()
+	private void remove(DTNode<L, T> top)
 	{
+		if(keepHubs)
+			for (DTLink<L, T> link : top.links())
+			{
+				// * ignore self-links
+				if (link.from().equals(link.to()))
+					continue;
+	
+				DTNode<L, T> neighbor = link.other(top);
+				DTNode<L, T> newNode = graph.add(top.label());
+	
+				if (link.from().equals(top))
+					newNode.connect(neighbor, link.tag());
+				else
+					neighbor.connect(newNode, link.tag());
+	
+				link.remove();
+			}
 
-		
+		top.remove();
 	}
 	
 	/**
